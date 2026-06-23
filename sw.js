16132b257390798680c6ef5b5281a755ad598596
agencesl68 <agence.sl.68@gmail.com>
2026-06-23 14:19:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker — cache l'app pour qu'elle s'ouvre même sans réseau
-const CACHE = "hk-carburant-v7";
+const CACHE = "hk-carburant-v9";
 const ASSETS = [
   "./",
   "./index.html",
